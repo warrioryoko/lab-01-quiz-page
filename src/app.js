f8a57088c5772658a7e469993f10780c7310fe26
warrioryoko <miangpellegri@gmail.com>
2020-07-06 15:50:14 -0700

    Added event listener for quiz button

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
+import {translateToAYes} from './translate-to-a-yes/translate-to-a-yes.js';
+
+const startQuizButton = document.getElementById('start-quiz-button');
 
 // initialize state
 
 // set event listeners to update state and DOM
+
+startQuizButton.onclick = () => {
+  alert('We are about to start the quiz!');
+  const quizConfirm = confirm('Would you like to start the quiz?');
+}
