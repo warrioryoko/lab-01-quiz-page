11fd887313d27e29436eafda1b512051994b7d30
warrioryoko <miangpellegri@gmail.com>
2020-07-06 17:27:20 -0700

    Finished all basic requirements except for questions.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,46 @@
 import {translateToAYes} from './translate-to-a-yes/translate-to-a-yes.js';
 
 const startQuizButton = document.getElementById('start-quiz-button');
-
+const displayResults = document.getElementById('score-goes-here');
 // initialize state
+
+
 
 // set event listeners to update state and DOM
 
 startQuizButton.onclick = () => {
+  let numberCorrect = 0;
+
   alert('We are about to start the quiz!');
-  const quizConfirm = confirm('Would you like to start the quiz?');
+  const userName = prompt('tell me your name: ');
+  const quizStartConfirm = confirm(`Well, ${userName}, would you like to start the quiz?`);
+  console.log(quizStartConfirm);
+
+  if (quizStartConfirm) {
+    const userAnswer1 = prompt(`Okay ${userName}, first question (yes/no)`);
+      if (userAnswer1 === 'y') {
+        numberCorrect++;
+      }
+    
+    const userAnswer2 = prompt(`Here is the second question, ${userName} (yes/no)`);
+    if (userAnswer2 === 'y') {
+      numberCorrect++;
+    }
+
+    const userAnswer3 = prompt(`Get ready ${userName}, here is the third question (yes/no)`);
+    if (userAnswer3 === 'n') {
+      numberCorrect++;
+    }
+
+    console.log(numberCorrect);
+
+    const quizEndConfirm = alert(`Alright, ${userName} we are done. Your results will be displayed below!`);
+
+    let resultsString = `Hey ${userName}, your score is ${numberCorrect} out of 3.`
+    
+    displayResults.textContent = resultsString;
+
+  } else {
+    return;
+  }
 }
